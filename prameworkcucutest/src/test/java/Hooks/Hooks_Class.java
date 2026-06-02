e4b2f0faf329e0d4435dce851470733c2dea75eb
suryaprakashgitj <113435133+suryaprakashgithubj@users.noreply.github.com>
2026-06-02 17:30:18 +0530

    s

--- prameworkcucutest/src/test/java/Hooks/Hooks_Class.java
+++ prameworkcucutest/src/test/java/Hooks/Hooks_Class.java
@@ -11,34 +11,31 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks_Class {
-
     public static WebDriver driver;
 
     @Before
-    public void beforeTest() {
-        try {
-            ChromeOptions options = new ChromeOptions();
-            
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--disable-logging");
+    public void beforeTest() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
 
-            String remoteUrl = System.getProperty("selenium.remote.url", 
-                                "http://selenium-chrome:4444/wd/hub");
+        String remoteUrl = System.getProperty(
+            "selenium.remote.url",
+            "http://selenium-chrome:4444"
+        );
 
-            driver = new RemoteWebDriver(new URL(remoteUrl), options);
+        driver = new RemoteWebDriver(new URL(remoteUrl), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://tutorialsninja.com/demo/");
+    }
 
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get("https://tutorialsninja.com/demo/");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to initialize WebDriver", e);
+    @After
+    public void afterTest() {
+        if (driver != null) {
+            driver.quit();
         }
     }
 }
