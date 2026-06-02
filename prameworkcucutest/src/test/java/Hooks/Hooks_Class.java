ee314c908922dda0a12fee6ddabc6a90ead6487d
suryaprakashgitj <113435133+suryaprakashgithubj@users.noreply.github.com>
2026-06-02 17:58:58 +0530

    s

--- prameworkcucutest/src/test/java/Hooks/Hooks_Class.java
+++ prameworkcucutest/src/test/java/Hooks/Hooks_Class.java
@@ -11,11 +11,14 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks_Class {
+
     public static WebDriver driver;
 
     @Before
     public void beforeTest() throws Exception {
+
         ChromeOptions options = new ChromeOptions();
+
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
@@ -23,17 +26,21 @@ public class Hooks_Class {
         options.addArguments("--remote-allow-origins=*");
 
         String remoteUrl = System.getProperty(
-            "selenium.remote.url",
-            "http://selenium-chrome:4444"
+                "selenium.remote.url",
+                "http://host.docker.internal:4444/wd/hub"
         );
 
         driver = new RemoteWebDriver(new URL(remoteUrl), options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("https://tutorialsninja.com/demo/");
     }
 
     @After
     public void afterTest() {
+
         if (driver != null) {
             driver.quit();
         }
